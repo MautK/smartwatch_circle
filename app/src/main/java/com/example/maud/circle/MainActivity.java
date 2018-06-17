b203f2ae72ea7a676059b5f3e4aef99401406130
MautK <maud.canisius@gmail.com>
2018-06-17 18:46:04 +0200

    include right sensor

--- app/src/main/java/com/example/maud/circle/MainActivity.java
+++ app/src/main/java/com/example/maud/circle/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.maud.circle;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.usage.UsageEvents;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.EventListener;
@@ -30,35 +20,22 @@ public class MainActivity extends WearableActivity {
     public SensorManager mSensorManager;
     private Sensor mGyroscope;
     private Sensor mMagnetometer;
-    private SensorActivity mSensorActivity;
+    private Sensor mRotation;
+
     private EventListener mEvent;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setContentView(new MyView(this));
-
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        //do we have this one the smartwatch?
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mRotation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
-
-
-        //to get all the different sensor types
-        final List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor type : deviceSensors) {
-            Log.e("sensors", type.getStringType());
-            //geomagnetic sensor used for users orientation (if accelerometer is not present)
-            //returns x,y,z for each sensor event
-            //rotational vector sensor is ideal for compasses
-
-
-        }
+        SensorActivity mSensorActivity = new SensorActivity();
     }
 
 }
--- app/src/main/java/com/example/maud/circle/SensorActivity.java
+++ app/src/main/java/com/example/maud/circle/SensorActivity.java
@@ -1,23 +1,38 @@
 package com.example.maud.circle;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class SensorActivity extends Activity implements SensorEventListener {
 
     public SensorManager mSensorManager;
-    private Sensor mMagneticSensor;
+    private Sensor mGyroscope;
+    private Sensor mMagnetometer;
+    private Sensor mRotation;
     int compass;
     float [] rMat = new float[9];
     float [] orientation = new float[9];
 
+    private double x = 0.0;
+    private double y = 0.0;
+    private double z = 0.0;
+
+    String TAG = "empty";
+
     @Override
     protected void onResume(){
         super.onResume();
-        mSensorManager.registerListener(this, mMagneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        int d = Log.d(TAG, "setData: yeah");
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mRotation, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
     }
 
     @Override
@@ -29,6 +44,7 @@ public class SensorActivity extends Activity implements SensorEventListener {
     //as little action as possible within this function
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "onSensorChanged: yeah");
         //check first which sensor is getting data
         if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
             //store data in matrix
@@ -36,6 +52,9 @@ public class SensorActivity extends Activity implements SensorEventListener {
             // some math calculations from this video
             // https://www.youtube.com/watch?v=nOQxq2YpEjQ
             compass = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]+360)%360);
+            //display data
+            setData(event.values[0], event.values[1], event.values[2]);
+
         }
     }
 
@@ -43,5 +62,11 @@ public class SensorActivity extends Activity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // more on the gyroscope, not sure if we can use this since it reports the rate of orientation but not according to where the north is
         // https://developer.android.com/guide/topics/sensors/sensors_motion
+    }
+
+    void setData(float newX, float newY, float newZ){
+        x = newX;
+        y = newY;
+        z = newZ;
     }
 }
