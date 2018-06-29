eb2e984579224433044f910c8c1cdadf1a18a5ca
maud canisius <maud@eduroam-057-024.scc.uni-weimar.de>
2018-06-29 14:00:46 +0200

    manual canvasDraw

--- app/src/main/java/com/example/maud/circle/MyView.java
+++ app/src/main/java/com/example/maud/circle/MyView.java
@@ -15,34 +15,38 @@ import java.util.List;
 
 public class MyView extends View
 {
-Paint paint = null;
-     public int x,y,rad;
-     public float turnDegrees;
+    Paint paint;
+    Canvas canvas;
+    public int x,y,rad;
+    public float turnDegrees;
 
     public MyView(Context context)
     {
         super(context);
         paint = new Paint();
+//        canvas = new Canvas();
+
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    protected void onDraw(Canvas blaCanvas) {
+        super.onDraw(blaCanvas);
         x = 50;
         y = 100;
         rad = 20;
-
+        canvas = blaCanvas;
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.WHITE);
         canvas.drawPaint(paint);
+        setDegrees(-0);
         paint.setColor(Color.parseColor("#CD5C5C"));
-        canvas.rotate(-20);
         canvas.drawCircle(x, y, rad, paint);
     }
 
 
     void setDegrees(float newDegrees) {
+        canvas.rotate(newDegrees);
         //both newDegrees and turnDegrees are working fine
-        turnDegrees = newDegrees;
+//        turnDegrees = newDegrees;
     }
 }
